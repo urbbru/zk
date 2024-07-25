5befd2228e18387d4bb647eb1c514f1dcc50de76
tim kryha <tim@kryha.io>
2024-07-25 14:44:02 +0200

    update BSN test

--- src/components/Gegevens/BSN.test.ts
+++ src/components/Gegevens/BSN.test.ts
@@ -3,14 +3,23 @@ import { mount } from '@vue/test-utils';
 import Gegevens from './Gegevens.vue';
 
 describe('BSN should be validated correctly', () => {
-    it('Should render correctly', () => {
+    it('Should show error message if incorrect BSN', () => {
         const wrapper = mount(Gegevens);
         const bsnInput = wrapper.find('input.bsn');
         bsnInput.setValue('incorrect bsn');
         bsnInput.trigger('blur');
 
-        expect(wrapper.find('span.bsn-error').text()).toEqual(
+        expect(wrapper.find('span').text()).toContain(
             'Helaas is het ingevoerde burgerservicenummer niet geldig. Probeer het opnieuw.'
         );
+    });
+
+    it('Should not show error message if correct BSN', () => {
+        const wrapper = mount(Gegevens);
+        const bsnInput = wrapper.find('input.bsn');
+        bsnInput.setValue('123456789');
+        bsnInput.trigger('blur');
+
+        expect(wrapper.find('span.bsn-error')).toBeNull();
     });
 });
